eea9f24a415da7071d193032db8bb7f0d5d36393
hei <32mens@gmail.com>
2016-08-16 18:31:16 +0900

    init test add

--- init_test.go
+++ init_test.go
@@ -1,9 +1,31 @@
 package tempcube
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestInit(t *testing.T) {
+	p := "sample_project"
+	err := Init(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(p)
 
+	// sample_project case init files
+	fs := []string{
+		fmt.Sprintf("%s/%s.go", p, p),
+		fmt.Sprintf("%s/%s_helper.go", p, p),
+		fmt.Sprintf("%s/%s_test.go", p, p),
+		fmt.Sprintf("%s/%s_helper_test.go", p, p),
+	}
+
+	for _, f := range fs {
+		_, err = os.Stat(f)
+		if err != nil {
+			t.Error(err)
+		}
+	}
 }
